aeeba787e155f5343825a5957bd7088e00d2074e
Michael Yan <rain@rainboyan.com>
2023-02-03 14:01:04 +0800

    Fixes STC error in AbstractHibernateGormStaticApi

--- grails-datastore-gorm-hibernate5/src/main/groovy/org/grails/orm/hibernate/AbstractHibernateGormStaticApi.groovy
+++ grails-datastore-gorm-hibernate5/src/main/groovy/org/grails/orm/hibernate/AbstractHibernateGormStaticApi.groovy
@@ -772,7 +772,7 @@ abstract class AbstractHibernateGormStaticApi<D> extends GormStaticApi<D> {
 
     protected List<String> removeNullNames(Map query) {
         List<String> nullNames = []
-        Set<String> allNames = new HashSet<String>(query.keySet())
+        Set<String> allNames = new HashSet<String>((Set<String>) query.keySet())
         for (String name in allNames) {
             if (query[name] == null) {
                 query.remove name
