07d7085916f195fded6bc28069b03b3bc3ab9070
Michael Yan <rain@rainboyan.com>
2023-04-14 15:39:15 +0800

    Examples: migrate to Grails 2022

--- examples/grails3-partitioned-multi-tenancy/grails-app/init/datasources/Application.groovy
+++ examples/grails3-partitioned-multi-tenancy/grails-app/init/datasources/Application.groovy
@@ -1,13 +1,12 @@
 package datasources
 
-import grails.boot.GrailsApp
-import grails.boot.config.GrailsAutoConfiguration
+import grails.boot.Grails
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 
 //@EnableAutoConfiguration(exclude = DataSourceTransactionManagerAutoConfiguration)
-class Application extends GrailsAutoConfiguration {
+class Application {
     static void main(String[] args) {
-        GrailsApp.run(Application)
+        Grails.run(Application)
     }
 }
