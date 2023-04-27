4b527984bd4e2731f2fe76c41f261b3e35646daf
Michael Yan <rain@rainboyan.com>
2023-04-27 13:57:01 +0800

    Migrate to Jakarta in all Examples

--- examples/grails-data-service/src/main/groovy/example/LoginAuthenticationSucessHandler.groovy
+++ examples/grails-data-service/src/main/groovy/example/LoginAuthenticationSucessHandler.groovy
@@ -1,23 +1,23 @@
 package example
 
-import grails.events.EventPublisher
-import grails.plugin.springsecurity.rest.RestAuthenticationSuccessHandler
+// import grails.events.EventPublisher
+// import grails.plugin.springsecurity.rest.RestAuthenticationSuccessHandler
 import groovy.transform.CompileStatic
-import org.springframework.security.core.Authentication
+// import org.springframework.security.core.Authentication
 
-import javax.servlet.ServletException
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+// import javax.servlet.ServletException
+// import javax.servlet.http.HttpServletRequest
+// import javax.servlet.http.HttpServletResponse
 
 @CompileStatic
-class LoginAuthenticationSucessHandler extends RestAuthenticationSuccessHandler implements EventPublisher {
+class LoginAuthenticationSucessHandler /*extends RestAuthenticationSuccessHandler implements EventPublisher*/ {
 
     TestService testService
 
-    @Override
-    void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        super.onAuthenticationSuccess(request, response, authentication)
-        testService.testDataService(1l)
-    }
+    // @Override
+    // void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    //     super.onAuthenticationSuccess(request, response, authentication)
+    //     testService.testDataService(1l)
+    // }
 
 }
