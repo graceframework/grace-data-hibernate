697f9555d40dda7deab3ad2cafbbc5ca5d7b73ff
Michael Yan <rain@rainboyan.com>
2025-02-11 16:11:24 +0800

    Migrate examples to Grace
    Not completed
    
    See gh-86

--- examples/spring-boot-hibernate5/src/main/groovy/example/BookController.groovy
+++ examples/spring-boot-hibernate5/src/main/groovy/example/BookController.groovy
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+import grails.gorm.transactions.Transactional
+
 @RestController
 @CompileStatic
 class BookController {
@@ -13,6 +15,7 @@ class BookController {
     @Autowired
     BookService bookService
 
+    @Transactional
     @RequestMapping("/books")
     List<Book> books() {
         Book.list()
