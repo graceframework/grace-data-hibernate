e43cc008e1785b4a7f24da823ee0a570a154bce2
Michael Yan <rain@rainboyan.com>
2025-05-15 17:26:14 +0800

    Prohibit executing integration test cases in examples
    Because webdriver-binaries-gradle-plugin does not support Gradle 8.6+
    
    Closes gh-110
    
    See gh-86

--- examples/grace-schema-per-tenant/src/integration-test/resources/GebConfig.groovy
+++ examples/grace-schema-per-tenant/src/integration-test/resources/GebConfig.groovy
@@ -2,14 +2,14 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxOptions
-import org.openqa.selenium.safari.SafariDriver
+// import org.openqa.selenium.safari.SafariDriver
 
 environments {
 
     // You need to configure in Safari -> Develop -> Allowed Remote Automation
-    safari {
-        driver = { new SafariDriver() }
-    }
+    // safari {
+    //     driver = { new SafariDriver() }
+    // }
 
     // run via “./gradlew -Dgeb.env=chrome iT”
     chrome {
