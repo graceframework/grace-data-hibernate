6d9666d291d021ccf52e7578444255873d79ca87
Michael Yan <rain@rainboyan.com>
2026-03-01 00:23:26 +0800

    Introduce HibernateEntityTraitProvider for Hibernate GORM
    Closes gh-145

---
+++ grace-datastore-gorm-hibernate/src/main/groovy/org/grails/orm/hibernate/HibernateEntityTraitProvider.groovy
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.grails.orm.hibernate
+
+import groovy.transform.CompileStatic
+
+import grails.gorm.hibernate.HibernateEntity
+
+import org.grails.compiler.gorm.GormEntityTraitProvider
+import org.grails.datastore.mapping.reflect.ClassUtils
+
+/**
+ * Tells GORM to use the {@link HibernateEntity} trait for Hibernate entities
+ *
+ * @author Michael Yan
+ * @since 2024.0.0
+ */
+@CompileStatic
+class HibernateEntityTraitProvider implements GormEntityTraitProvider {
+
+    final Class entityTrait = HibernateEntity
+
+    final boolean available = ClassUtils.isPresent('org.hibernate.Session')
+
+}
